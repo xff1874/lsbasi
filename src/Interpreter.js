e1a58a7e23d68e139ef87d3e73d768d575351c76
zwd <zwd321081@gmail.com>
2019-04-18 17:31:06 +0800

    feat: p4 split code into Lexer and Interpreter js

--- src/Interpreter.js
+++ src/Interpreter.js
@@ -2,103 +2,9 @@ import TokenType from './TokenType';
 import Token from './Token';
 
 class Interpreter {
-    constructor(text) {
-        this.text = text;
-        this.pos = 0;
-        this.current_token = null;
-        //add current_char field;
-        this.current_char = this.text[this.pos];
-    }
-
-    error() {
-        throw new Error('Error parsing input');
-    }
-    isdigital(c) {
-        let re = /\d/g;
-        return re.test(c);
-    }
-
-    isspace(c) {
-        return /\s/g.test(c);
-    }
-
-    skip_whitespace() {
-        while (this.current_char != null && this.isspace(this.current_char)) {
-            this.advance();
-        }
-    }
-
-    //Return a (multidigit) integer consumed from the input.
-    integer() {
-        //not end and is digital loop
-        let re = '';
-        while (this.current_char != null && this.isdigital(this.current_char)) {
-            re += this.current_char;
-            this.advance();
-        }
-        return re;
-    }
-
-    //get next token;
-    get_next_token() {
-        while (this.current_char) {
-            if (this.isspace(this.current_char)) {
-                this.skip_whitespace();
-                continue;
-            }
-
-            if (this.isdigital(this.current_char)) {
-                return new Token(TokenType.INTEGER, this.integer());
-            }
-
-            if (this.current_char === '+') {
-                this.advance();
-                return new Token(TokenType.PLUS, '+');
-            }
-
-            if (this.current_char === '-') {
-                this.advance();
-                return new Token(TokenType.MINUS, '-');
-            }
-
-            if (this.current_char === '*') {
-                this.advance();
-                return new Token(TokenType.MUL, '*');
-            }
-
-            if (this.current_char === '/') {
-                this.advance();
-                return new Token(TokenType.DIVISION, '/');
-            }
-
-            this.error();
-        }
-
-        return new Token(TokenType.EOF, null);
-    }
-    /*compare current token type with passed token type
-     * if matched then return the next_token
-     * else throw error;
-     */
-
-    eat(token_type) {
-        if (this.current_token.type === token_type) {
-            this.current_token = this.get_next_token();
-        } else throw new Error(`${token_type} is not matched`);
-    }
-
-    //move the pos pointer and set the current_char
-    advance() {
-        this.pos++;
-        if (this.pos > this.text.length - 1) {
-            this.current_char = null; // represent the end character;
-        } else this.current_char = this.text[this.pos];
-    }
-
-    term(type) {
-        let ct = this.current_token;
-        this.eat(type);
-        return parseInt(ct.value);
+    constructor(lexer) {
+        this.lexer = lexer;
+        this.current_token = this.lexer.get_next_token();
     }
 
     factor() {
@@ -107,8 +13,18 @@ class Interpreter {
         return parseInt(current_token.value);
     }
 
+    /*compare current token type with passed token type
+     * if matched then return the next_token
+     * else throw error;
+     */
+
+    eat(token_type) {
+        if (this.current_token.type === token_type) {
+            this.current_token = this.lexer.get_next_token();
+        } else throw new Error(`${token_type} is not matched`);
+    }
+
     expr() {
-        this.current_token = this.get_next_token();
         let re = this.factor();
 
         while (
--- src/test.js
+++ src/test.js
@@ -1,9 +1,11 @@
 import Interpreter from './Interpreter';
+import Lexer from './Lexer';
 
 function run() {
     debugger;
-    let interp = new Interpreter('7 * 4 / 2');
-    let re = interp.expr();
+    let lexer = new Lexer('7 * 4 / 2');
+    let inp = new Interpreter(lexer);
+    let re = inp.expr();
     console.log(re);
 }
 
