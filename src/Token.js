53e63f52c27f60833cf8588b05568dc895acd6fe
zwd <zwd321081@gmail.com>
2019-04-22 09:41:17 +0800

    feat: remove all finished and wait to pratice

--- src/Token.js
+++
@@ -1,9 +0,0 @@
-export default class Token {
-    constructor(type, value) {
-        this.type = type;
-        this.value = value;
-    }
-    toString() {
-        console.log(`Token(${this.thype},${this.value})`);
-    }
-}
